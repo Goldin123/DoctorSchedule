f9369341d497d9db6004d88deada9a2d950fc8d5
Goldin Baloyi <goldin.baloyi@gitechnologies.co.za>
2024-11-13 12:03:18 +0200

    registered EF on the main api.

--- DoctorSchedule/Program.cs
+++ DoctorSchedule/Program.cs
@@ -1,3 +1,11 @@
+using DoctorSchedule.Application.Messaging.Implementation;
+using DoctorSchedule.Application.Messaging.Interface;
+using DoctorSchedule.Domain.RepositoriesInterface;
+using DoctorSchedule.Infrastructure.Persistence;
+using DoctorSchedule.Infrastructure.RepositoriesImplementation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure EF Core with SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        b => b.MigrationsAssembly("DoctorSchedule.Infrastructure")));
+
+
+builder.Services.AddScoped<IEventRepository,EventRepository>();
+builder.Services.AddScoped<IMessageQueue, InMemoryMessageQueue>();
 
 var app = builder.Build();
 
