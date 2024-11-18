79bfb090d2f256704600ed8d06abb35fa630b8c3
Goldin Baloyi <goldin.baloyi@gitechnologies.co.za>
2024-11-18 17:05:40 +0200

    removed auth limits

--- DoctorSchedule.Application/Queries/LoginUserQuery.cs
+++ DoctorSchedule.Application/Queries/LoginUserQuery.cs
@@ -10,11 +10,9 @@ namespace DoctorSchedule.Application.Queries
     public class LoginUserQuery
     {
         [Required(ErrorMessage = "Username is required.")]
-        [StringLength(50, ErrorMessage = "Username cannot exceed 50 characters.")]
         public string? Username { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
-        [StringLength(150, ErrorMessage = "Password cannot exceed 150 characters.")]
         public string? Password { get; set; }
     }
 }
